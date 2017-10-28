cec71d34f6cfebd70e3dc618378ca2cb166e3f96
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2017-10-28 13:44:36 +0300

    fix #1

--- main.go
+++ main.go
@@ -27,26 +27,27 @@ type Task struct {
 	value []byte
 }
 
+var memc map[string] string
+var pattern *string
+var workers *int
+
+func init() {
+	memc = make(map[string]string)
+	pattern = flag.String("pattern", "", "Files pattern.")
+	memc["idfa"] = *flag.String("idfa", "127.0.0.1:33013", "Memcache for idfa.")
+	memc["gaid"] = *flag.String("gaid", "127.0.0.1:33014", "Memcache for gaid.")
+	memc["adid"] = *flag.String("adid", "127.0.0.1:33015", "Memcache for adid.")
+	memc["dvid"] = *flag.String("dvid", "127.0.0.1:33016", "Memcache for dvid.")
+	workers = flag.Int("workers", runtime.NumCPU(), "Count of forkers.")
+}
+
 func main() {
-	memc := make(map[string]string)
-	pattern := flag.String("pattern", "", "Files pattern.")
-	idfaOpt := flag.String("idfa", "127.0.0.1:33013", "Memcache for idfa.")
-	gaidOpt := flag.String("gaid", "127.0.0.1:33014", "Memcache for gaid.")
-	adidOpt := flag.String("adid", "127.0.0.1:33015", "Memcache for adid.")
-	dvidOpt := flag.String("dvid", "127.0.0.1:33016", "Memcache for dvid.")
-	workers := flag.Int("workers", runtime.NumCPU(), "Count of forkers.")
-	fmt.Println(*pattern, *idfaOpt, *gaidOpt, *adidOpt, *dvidOpt, *workers)
 	flag.Parse()
-	if *pattern == "" {
-		log.Fatal("Pattern not found in arguments")
-	}
 	if flag.NFlag() == 0 {
 		flag.PrintDefaults()
-	} else {
-		memc["idfa"] = *idfaOpt
-		memc["gaid"] = *gaidOpt
-		memc["adid"] = *adidOpt
-		memc["dvid"] = *dvidOpt
+	}
+	if *pattern == "" {
+		log.Fatal("Pattern not found in arguments")
 	}
 	runtime.GOMAXPROCS(*workers)
 	err := start(pattern, &memc)
